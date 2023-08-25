9cc19e7153981684372d2f20f6afe57c6b2e8d6a
Luca Conesa <conesa.luca1997@gmail.com>
2023-08-25 16:53:12 -0300

    garegue registrarse.html y cambie varias cosas en el css

--- script.js
+++ script.js
@@ -84,3 +84,16 @@ submitButton.addEventListener('click', () => {
     document.getElementById('username').value = '';
     document.getElementById('user-comment').value = '';
 });
+// boton submit en sobre nosotros
+document.addEventListener('DOMContentLoaded', () => {
+    const submitButton = document.getElementById('submit-button');
+
+    submitButton.addEventListener('click', () => {
+        submitButton.classList.add('loading');
+
+        // Simula una acción de carga
+        setTimeout(() => {
+            submitButton.classList.remove('loading');
+        }, 2000); // Cambia esto al tiempo real de tu acción de carga
+    });
+});
